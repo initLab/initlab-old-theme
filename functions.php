21208470d25be74f301e14cdd9c6c3eb5bcfc347
Konstantin Dankov <konstantin.dankov@gmail.com>
2011-06-03 14:06:35 +0100

    All post types are now shown in the homepage loop.

--- functions.php
+++ functions.php
@@ -5,12 +5,12 @@
 	function my_get_posts( $query ) {
 
 		if ( is_home() && false == $query->query_vars['suppress_filters'] )
-			$query->set( 'post_type', array( 'post', 'events' ) );
+			$query->set( 'post_type', array( 'post', 'events', 'articles', 'events', 'projects', 'courses' ) );
 
 		return $query;
 	}
-	// add_filter( 'pre_get_posts', 'my_get_posts' );
-	
+	add_filter( 'pre_get_posts', 'my_get_posts' );
+
 	function remove_wp_widget_recent_comments_style() {
 	      remove_filter('wp_head', 'wp_widget_recent_comments_style');
 	}
@@ -21,6 +21,6 @@
 		echo '<script src="/wp-content/themes/initlab/scripts/common.js"></script>';
 	}
 	add_action('wp_head', 'import_scripts');
-	
-	
+
+
 ?>
