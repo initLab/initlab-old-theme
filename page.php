8406d558c86f1af6656e462c3ce9ff7d53f760da
Konstantin Dankov <konstantin.dankov@gmail.com>
2011-06-06 23:54:32 +0100

    Support for removing the sidebar with a custom field 'sidebarless'

---
+++ page.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage K2
+ * @since K2 unknown
+ */
+
+get_header(); ?>
+
+<div class="wrapper">
+	<?php if ( is_active_sidebar('widgets-top') ) : ?>
+	<div id="widgets-top" class="widgets">
+		<?php dynamic_sidebar('widgets-top'); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="primary">
+		<a name="startcontent"></a>
+
+		<?php /* K2 Hook */ do_action('template_primary_begin'); ?>
+
+		<div class="content hfeed">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+			<div id="entry-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="entry-header">
+					<h1 class="entry-title">
+						<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php k2_permalink_title(); ?>"><?php the_title(); ?></a>
+					</h1>
+
+					<?php /* Edit Link */ edit_post_link(__('Edit', 'k2'), '<span class="entry-edit">', '</span>'); ?>
+
+					<?php /* K2 Hook */ do_action('template_entry_head'); ?>
+				</div><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div><!-- .entry-content -->
+
+				<div class="entry-footer">
+					<?php wp_link_pages( array('before' => '<div class="entry-pages"><span>' . __('Pages:', 'k2') . '</span>', 'after' => '</div>' ) ); ?>
+
+					<?php /* K2 Hook */ do_action('template_entry_foot'); ?>
+				</div><!-- .entry-footer -->
+			</div><!-- #entry-ID -->
+
+			<?php if ( comments_open() ): ?>
+			<div class="comments">
+				<?php comments_template(); ?>
+			</div><!-- .comments -->
+			<?php endif; ?>
+
+		<?php endwhile; else: define('K2_NOT_FOUND', true); ?>
+
+			<?php locate_template( array('blocks/k2-404.php'), true ); ?>
+
+		<?php endif; ?>
+
+		</div><!-- .content -->
+
+		<?php /* K2 Hook */ do_action('template_primary_end'); ?>
+
+	</div><!-- .primary -->
+
+	<?php if ( ! get_post_custom_values('sidebarless') ) get_sidebar(); ?>
+
+	<?php if ( is_active_sidebar('widgets-bottom') ) : ?>
+	<div id="widgets-bottom" class="widgets">
+		<?php dynamic_sidebar('widgets-bottom'); ?>
+	</div>
+	<?php endif; ?>
+
+</div><!-- .wrapper -->
+
+<?php get_footer(); ?>
