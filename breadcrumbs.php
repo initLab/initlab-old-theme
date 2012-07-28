e28c5cd7cc374f46b93a0207596884a1fa7e7eae
Konstantin Dankov <konstantin.dankov@gmail.com>
2012-07-28 13:02:02 +0100

    Fixed breadcrumbs. Its a selfcontained function now

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -1,11 +1,15 @@
 <?php
 
-	function breadcrumbs( $type, $single ) {
+	function breadcrumbs() {
+
+		global $wp_query;
+
+		$type = $wp_query->query_vars[post_type];
 
 		$link[] = array( 'Home', '/' );
-		$link[] = array( $type, '/'.$type );
 
-		if($single){ $link[] = array( 'Permalink', get_permalink() ); }
+		if($type){ $link[] = array( ucfirst($type), '/'.$type ); }
+		if(is_single()){ $link[] = array( get_the_title(), get_permalink() ); }
 
 		echo '<div class="breadcrumbs">';
 
