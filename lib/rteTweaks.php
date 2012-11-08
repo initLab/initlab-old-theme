bd8dcd5aa176155f39dc714558462e4b4805c3b4
Konstantin Dankov <konstantin.dankov@gmail.com>
2012-11-08 13:00:40 +0200

    update

--- lib/rteTweaks.php
+++ lib/rteTweaks.php
@@ -12,4 +12,11 @@
 	}
 	add_filter("mce_buttons_3", "add_more_buttons");
 
+	// Remove With & Height attributes from images
+	function remove_width_attribute( $html ) {
+		$html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+		return $html;
+	}
+	add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+	add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
 ?>
